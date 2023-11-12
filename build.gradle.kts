c8c1ba83cec11e62dc703b6a98b5a1eff8003d7d
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2023-11-12 12:26:29 +0000

    Add initial multiplatform project

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    kotlin("multiplatform") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
+    application
+}
+
+group = "uk.co.developmentanddinosaurs.apps.poker"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    js(IR) {
+        binaries.executable()
+        browser {
+            distribution(Action {
+                outputDirectory = file("$projectDir/src/jvmMain/resources/web")
+            })
+            commonWebpackConfig(Action {
+                cssSupport {
+                    enabled.set(true)
+                }
+            })
+        }
+    }
+    jvm {
+        jvmToolchain(17)
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting
+        val jsMain by getting
+        val jsTest by getting
+        val jvmMain by getting
+        val jvmTest by getting
+    }
+}
+
+application {
+    mainClass.set("uk.co.developmentanddinosaurs.apps.poker.application.PokerServerKt")
+}
+
+tasks.named<Copy>("jvmProcessResources") {
+    val jsBrowserDistribution = tasks.named("jsBrowserDistribution")
+    from(jsBrowserDistribution)
+}
+
+tasks.named<JavaExec>("run") {
+    dependsOn(tasks.named<Jar>("jvmJar"))
+    classpath(tasks.named<Jar>("jvmJar"))
+}
+
+tasks.named<Jar>("jvmJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "uk.co.developmentanddinosaurs.apps.poker.application.PokerServerKt"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
