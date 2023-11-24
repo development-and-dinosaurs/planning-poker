20e52af20acc0306a176405b35ccc785730ae032
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2023-11-24 07:54:00 +0000

    Add button animation
    This requires the materialize javascript library to work

--- src/jvmMain/kotlin/uk/co/developmentanddinosaurs/apps/poker/application/html/components/Head.kt
+++ src/jvmMain/kotlin/uk/co/developmentanddinosaurs/apps/poker/application/html/components/Head.kt
@@ -20,6 +20,10 @@ fun HTML.head(title: String) = run {
             type = "text/javascript"
             src = "/planning-poker.js"
         }
+        script {
+            type = "text/javascript"
+            src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
+        }
         title(title)
         meta {
             name = "viewport"
