4b3c2a9946b2fe014a126ebf615b6d8b4939d17d
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2023-11-25 16:33:45 +0000

    Use linux line endings
    We've been splitting the file based on building it on Windows, but now
    that we're using GitHub actions the file is being built on Linux.
    
    The old line endings don't work there, but this fixes it.

--- src/jvmMain/kotlin/uk/co/developmentanddinosaurs/apps/poker/application/services/NameGenerator.kt
+++ src/jvmMain/kotlin/uk/co/developmentanddinosaurs/apps/poker/application/services/NameGenerator.kt
@@ -1,8 +1,8 @@
 package uk.co.developmentanddinosaurs.apps.poker.application.services
 
 class NameGenerator {
-    private val adjectives = NameGenerator::class.java.getResource("/adjectives.txt").readText().split("\r\n")
-    private val dinosaurs = NameGenerator::class.java.getResource("/dinosaurs.txt").readText().split("\r\n")
+    private val adjectives = NameGenerator::class.java.getResource("/adjectives.txt").readText().split("\n")
+    private val dinosaurs = NameGenerator::class.java.getResource("/dinosaurs.txt").readText().split("\n")
     fun generateName(): String {
         return adjectives.random() + " " + dinosaurs.random()
     }
