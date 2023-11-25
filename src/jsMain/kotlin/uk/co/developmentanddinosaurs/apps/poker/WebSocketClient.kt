4fd7e8a181032b902bd17b082aea9b331556cca2
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2023-11-25 10:16:42 +0000

    Use private variable

--- src/jsMain/kotlin/uk/co/developmentanddinosaurs/apps/poker/WebSocketClient.kt
+++ src/jsMain/kotlin/uk/co/developmentanddinosaurs/apps/poker/WebSocketClient.kt
@@ -11,7 +11,7 @@ import poker.events.Event
 class WebSocketClient(private val httpClient: HttpClient) {
     private lateinit var session: WebSocketSession
 
-    var isConnected = false
+    private var isConnected = false
 
     suspend fun connect() {
         val host = window.location.hostname
