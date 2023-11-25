bbbdee5e227a98c12daca442ff7bed1045f61e49
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2023-11-25 17:05:35 +0000

    Use system line separators
    This lets the app work regardless of where it's built

--- src/jvmMain/kotlin/uk/co/developmentanddinosaurs/apps/poker/application/services/NameGenerator.kt
+++ src/jvmMain/kotlin/uk/co/developmentanddinosaurs/apps/poker/application/services/NameGenerator.kt
@@ -1,8 +1,8 @@
 package uk.co.developmentanddinosaurs.apps.poker.application.services
 
 class NameGenerator {
-    private val adjectives = NameGenerator::class.java.getResource("/adjectives.txt").readText().split("\n")
-    private val dinosaurs = NameGenerator::class.java.getResource("/dinosaurs.txt").readText().split("\n")
+    private val adjectives = NameGenerator::class.java.getResource("/adjectives.txt").readText().split(System.lineSeparator())
+    private val dinosaurs = NameGenerator::class.java.getResource("/dinosaurs.txt").readText().split(System.lineSeparator())
     fun generateName(): String {
         return adjectives.random() + " " + dinosaurs.random()
     }
