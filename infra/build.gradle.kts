d8e3d9100bd323d5ab3786ce1d24137ab875f32f
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2023-12-02 15:29:49 +0000

    Add infra project
    This allows us to keep the infrastructure in the same repo,
    but not too close to the code.
    
    It didn't play nice in the multiplatform project anyway, so this is for the best.

---
+++ infra/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("kotlin")
+    id("com.diffplug.spotless") version "6.23.0"
+}
+
+group = "uk.co.developmentanddinosaurs.apps.poker"
+version = "1.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("software.amazon.awscdk:aws-cdk-lib:2.111.0")
+}
+
+tasks {
+    jar {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest {
+            attributes["Main-Class"] = "uk.co.developmentanddinosaurs.apps.poker.infra.MainKt"
+        }
+        from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    }
+}
