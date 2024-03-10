f476dac75f967eedb3458f6baa5d187dfdfb24c9
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-03-10 07:39:00 +0000

    Change main infra app name
    This is more obvious what it's supposed to be doing

--- infra/src/main/kotlin/uk/co/developmentanddinosaurs/apps/poker/infra/CdkApp.kt
+++ infra/src/main/kotlin/uk/co/developmentanddinosaurs/apps/poker/infra/CdkApp.kt
@@ -3,7 +3,7 @@ package uk.co.developmentanddinosaurs.apps.poker.infra
 import software.amazon.awscdk.App
 
 fun main() {
-    val app = App()
+    val cdkApp = App()
     PokerStack(app, "PlanningPokerApplication")
-    app.synth()
+    cdkApp.synth()
 }
