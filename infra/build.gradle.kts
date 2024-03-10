354700c295b0ace6dd1d08593f2ec797b5cc0095
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-03-10 08:48:02 +0000

    Upload to S3 from Gradle
    We had a hardcoded bucket name in the deploy yaml file, which wasn't ideal.
    
    This moves the S3 upload to the Gradle file, which reads the bucket
    name from the infrastructure properties file.

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileReader
+import java.util.Properties
+
 plugins {
     id("kotlin")
     id("com.diffplug.spotless")
@@ -20,5 +23,9 @@ tasks {
             attributes["Main-Class"] = "uk.co.developmentanddinosaurs.apps.poker.infra.CdkAppKt"
         }
         from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    }
+    register<Exec>("uploadToS3") {
+        val bucket = Properties().apply { load(FileReader("infra/src/main/resources/infra.properties")) }.getProperty("bucket-name")
+        commandLine("aws", "s3", "cp", rootProject.tasks.jar.get().outputs.files.singleFile, "s3://$bucket/planning-poker.jar")
     }
 }
