5dde7c9a10534483f04e7f66197c285bc080beb2
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-03-10 07:43:34 +0000

    Add stack configuration
    This allows us to keep the configurable items in a central place, making
    it easier to update.

---
+++ infra/src/main/kotlin/uk/co/developmentanddinosaurs/apps/poker/infra/config/Configuration.kt
@@ -0,0 +1,23 @@
+package uk.co.developmentanddinosaurs.apps.poker.infra.config
+
+import java.util.Properties
+
+object Configuration {
+    private val properties by lazy { Properties().apply { load(Configuration::class.java.getResourceAsStream("/infra.properties")) } }
+
+    val awsAccount: String =
+        properties.getProperty("aws-account")
+            ?: throw ConfigException("No AWS account found. Specify aws-account in infra.properties")
+    val awsRegion: String =
+        properties.getProperty("aws-region")
+            ?: throw ConfigException("No AWS region found. Specify aws-region in infra.properties")
+    val bucketName: String =
+        properties.getProperty("bucket-name")
+            ?: throw ConfigException("No bucket name found. Specify bucket-name in infra.properties")
+    val githubOrganization: String =
+        properties.getProperty("github-organization")
+            ?: throw ConfigException("No GitHub organization found. Specify github-organization in infra.properties")
+    val githubRepository: String =
+        properties.getProperty("github-repository")
+            ?: throw ConfigException("No GitHub repository found. Specify github-repository in infra.properties")
+}
