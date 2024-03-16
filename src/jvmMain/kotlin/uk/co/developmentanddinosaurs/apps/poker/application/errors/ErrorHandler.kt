32090827be90c4558a02abba3c4a710190645c53
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-03-16 16:01:32 +0000

    Add log statements for things
    This adds logging for things that can happen in the application,
    which is kind of cool.

--- src/jvmMain/kotlin/uk/co/developmentanddinosaurs/apps/poker/application/errors/ErrorHandler.kt
+++ src/jvmMain/kotlin/uk/co/developmentanddinosaurs/apps/poker/application/errors/ErrorHandler.kt
@@ -3,19 +3,29 @@ package uk.co.developmentanddinosaurs.apps.poker.application.errors
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.html.respondHtml
+import org.slf4j.LoggerFactory
 import uk.co.developmentanddinosaurs.apps.poker.application.html.pages.pageNotFound
 import uk.co.developmentanddinosaurs.apps.poker.application.html.pages.roomNotFound
 import uk.co.developmentanddinosaurs.apps.poker.application.html.pages.serverError
 import uk.co.developmentanddinosaurs.apps.poker.application.rooms.RoomDoesNotExistException
 
 class ErrorHandler {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
     suspend fun handle(
         call: ApplicationCall,
         cause: Throwable,
     ) {
         when (cause) {
-            is RoomDoesNotExistException -> call.respondHtml(HttpStatusCode.NotFound) { roomNotFound(cause.room) }
-            else -> call.respondHtml(HttpStatusCode.InternalServerError) { serverError() }
+            is RoomDoesNotExistException -> {
+                log.error("Someone tried to access room [${cause.room}] which does not exist")
+                call.respondHtml(HttpStatusCode.NotFound) { roomNotFound(cause.room) }
+            }
+            else -> {
+                log.error("Something bad happened", cause)
+                call.respondHtml(HttpStatusCode.InternalServerError) { serverError() }
+            }
         }
     }
 
