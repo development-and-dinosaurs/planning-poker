3ca67d2d2f303e2e1901b5eb57581d2a8015e38c
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-04-07 20:20:37 +0100

    Update dependencies

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("software.amazon.awscdk:aws-cdk-lib:2.134.0")
+    implementation("software.amazon.awscdk:aws-cdk-lib:2.136.0")
 }
 
 tasks {
