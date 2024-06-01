272d7f34dbe01ccb45b4fb6d6e1af4d4a7fce153
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-06-01 19:11:18 +0100

    :arrow_up: Update CDK to 2.144.0

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("software.amazon.awscdk:aws-cdk-lib:2.141.0")
+    implementation("software.amazon.awscdk:aws-cdk-lib:2.144.0")
 }
 
 tasks {
